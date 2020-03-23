824e765e742f8b3c19d19cbd73fc2ef139a10dc5
Dmitry Karmazin <dmitry@skbkontur.ru>
2020-03-23 16:47:37 +0500

    auth for snmp from env variables

--- Functions/plugins/SnmpThermometer.ps1
+++ Functions/plugins/SnmpThermometer.ps1
@@ -4,11 +4,20 @@ param([Hashtable]$GlobalConfig, [System.Xml.XmlElement]$ModuleConfig)
 function GetSnmpTemperature {
 param ([System.Xml.XmlElement]$ModuleConfig)
 
+if ((get-module -ListAvailable SNMP) -eq $null) {
+Write-Host "SNMP module not installed"
+return 
+}
+
 $sensors = $ModuleConfig.Sensor
 $sensors | %{
 $sensor = $_
 try {
-	$snmpData = Get-SnmpData -IP $sensor.Host -Community $ModuleConfig.Auth.Community -OID '1.3.6.1.4.1.40418.2.2.4.1.0'
+	$community = $ModuleConfig.Auth.Community
+	if ($ModuleConfig.Auth.Community.StartsWith('$env:')) {
+		$community = [environment]::GetEnvironmentVariable($ModuleConfig.Auth.Community.TrimStart('$env:'))
+	}
+	$snmpData = Get-SnmpData -IP $sensor.Host -Community $community -OID '1.3.6.1.4.1.40418.2.2.4.1.0'
 	[pscustomobject]@{ Path=$sensor.Name; Value=$snmpData.Data}
 	}
  catch {
