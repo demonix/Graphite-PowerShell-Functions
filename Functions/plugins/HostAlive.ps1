1dd4a2a5fff0d292ae03f330a6971f56eac0d633
alertmf <74922929+alertmf@users.noreply.github.com>
2021-09-14 14:01:02 +0500

    Рефакторинг HostAlive
    * Рефакторинг кода
    
    * check errors
    
    * Рефакторинг кода
    
    * fix errors
    
    * fix variable pingResults
    
    * Обход ошибки, если хост не резолвится
    
    * Обход ошибки, если хост не резолвится
    
    * Обход ошибки, если хост не резолвится
    
    * Обход ошибки, если хост не резолвится
    
    * edit config
    
    * Рефакторинг кода
    
    * Delete StatsToGraphiteConfig.xml
    
    * Add files via upload
    
    * Delete StatsToGraphiteConfig.xml
    
    * Add files via upload
    
    Co-authored-by: legotkin <legotkin@skbkontur.ru>

--- Functions/plugins/HostAlive.ps1
+++ Functions/plugins/HostAlive.ps1
@@ -1,9 +1,15 @@
 ﻿param([Hashtable]$GlobalConfig, [System.Xml.XmlElement]$ModuleConfig)
 
+
 function GetHostAliveAsync {
     param ([System.Xml.XmlElement]$ModuleConfig)
-
     $hosts = $ModuleConfig.Host.Name.ToLower();
+    $pingResults=GetPingResultAsync -hosts $hosts
+    GetMetricHostIsAlive -pingResults $pingResults
+    }
+
+function GetPingResultAsync {
+    param ($hosts)
 	$timeout = 300
 	
     #Запускается асинхронный пинг до хостов		
@@ -11,48 +17,62 @@ function GetHostAliveAsync {
         $task = [System.Net.NetworkInformation.Ping]::new().SendPingAsync($_,$timeout)
 		[pscustomobject]@{ Host=$_; Task=$task }
 	}
-	Start-Sleep -Milliseconds ($timeout*2)
+    
+    #Ожидание завершения SendPingAsync и игнорирование ошибки резолвинга хоста
+    Try {
+        [System.Threading.Tasks.Task]::WaitAll($tasks.task)
+    } 
+    catch {
+         Write-Warning ($_.exception.InnerException.InnerExceptions |%{$_.Message+" "+$_.InnerException.Message})
+        }
 
-	#Если пинг не отменен по таймауту и завершился успешно, то возвращется значение метрик IsAlive=1		
-	$tasks | ? { ($_.Task.IsCanceled -eq $false) -and ($_.Task.Result.Status -eq 'Success')}  | %{
-            GetHostMetricIsAlive -host $_.Host -NodeHostName $NodeHostName -Value 1
+    #Если хост не резолвится, то возвращается значение метрики IsAlive=0
+    $faultedtask=$tasks | ? {($_.Task.IsFaulted )}
+    $faultedtask | %{ 
+        [pscustomobject]@{ Host=$_.Host; Value=0 }
+    }
+    #Переопределяем хосты, отбрасываем хосты, которые не резолвятся
+    $tasks=$tasks | ? {($_.Task.IsFaulted -ne $true)}
+
+    #Если пинг не отменен по таймауту и завершился успешно, то возвращется значение метрики IsAlive=1	            
+    $tasks | ? { ($_.Task.IsCanceled -eq $false) -and ($_.Task.Result.Status -eq 'Success')}  | %{
+           [pscustomobject]@{ Host=$_.Host; Value=1 }
     }
 
 	#Запускается повтороный пинг для всех не успешных хостов 		
-	$secondTasks = $tasks | ? {($_.Task.IsCanceled) -or ($_.Task.Result.Status -ne 'Success')} | %{
+	$secondTasks = $tasks | ? { ($_.Task.IsCanceled) -or ($_.Task.Result.Status -ne 'Success')} | %{
 			$secondTask = [System.Net.NetworkInformation.Ping]::new().SendPingAsync($_.Host,$timeout)
 		[pscustomobject]@{ Host=$_.Host; Task=$secondTask }
 	}
-	Start-Sleep -Milliseconds ($timeout*2)
+    Try {
+        [System.Threading.Tasks.Task]::WaitAll($tasks.task)
+    } 
+    catch {
+         Write-Warning ($_.exception.InnerException.InnerExceptions |%{$_.Message+" "+$_.InnerException.Message})
+        }
 
     #Проверяется результат повторного пинга и возвращаются соответствующее значения метрик IsAlive
 	$secondTasks | ? {($_.Task.IsCanceled -eq $false) -and ($_.Task.Result.Status -eq 'Success')} | %{
-        GetHostMetricIsAlive -host $_.Host -NodeHostName $NodeHostName -Value 1
+        [pscustomobject]@{ Host=$_.Host; Value=1 }
     }
 	$secondTasks | ? {($_.Task.IsCanceled) -or ($_.Task.Result.Status -ne 'Success')}  | %{
-        GetHostMetricIsAlive -host $_.Host -NodeHostName $NodeHostName -Value 0
+        [pscustomobject]@{ Host=$_.Host; Value=0 }
     }			
 }
 
 
-function GetHostMetricIsAlive {
-    param (
-        $host,
-        $NodeHostName,
-        $Value
-    )  
-         
-    [pscustomobject]@{ 
-        Path="\\$host\IsAlive";
-        Value=$Value
-    }
+function GetMetricHostIsAlive {
+    param ( $pingResults )  
+    $pingResults | ForEach-Object {         
+    $HostMetric=$_.Host
+    $ValueMetric=$_.Value
+    [pscustomobject]@{Path="\\$HostMetric\IsAlive";Value="$ValueMetric"}
 
-    [pscustomobject]@{
-        Path="\\$host\$NodeHostName\IsAlive";
-        Value=$Value
+    [pscustomobject]@{Path="\\$HostMetric\$($env:COMPUTERNAME)\IsAlive".ToLower();Value="$ValueMetric"}
     }
 
    }
+
 
 function GetHostAlive {
 param ([System.Xml.XmlElement]$ModuleConfig)
@@ -82,7 +102,7 @@ $hosts = $ModuleConfig.Host.Name
 }
 
 $MetricPath = $GlobalConfig.MetricPath
-$NodeHostName = $GlobalConfig.NodeHostName.ToLower();
+$NodeHostName = $GlobalConfig.NodeHostName
 
 if ($ModuleConfig.HasAttribute("CustomPrefix"))
 {
