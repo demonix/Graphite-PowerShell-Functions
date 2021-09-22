020127325d5e0e4b8cb3965fc255c71efc38763d
alertmf <alert.mf@gmail.com>
2021-09-22 12:42:14 +0500

    Вынос функции

---
+++ Functions/plugins/GetMetricHostIsAlive.ps1
@@ -0,0 +1,15 @@
+﻿function GetMetricHostIsAlive {
+    param ( $pingResults )
+
+     if ($pingResults -eq $null){
+         $pingResults = @() } 
+
+    $pingResults | ForEach-Object {         
+    $HostMetric=$_.Host
+    $ValueMetric=$_.Value
+    [pscustomobject]@{Path="\\$HostMetric\IsAlive";Value="$ValueMetric"}
+
+    [pscustomobject]@{Path="\\$HostMetric\$(($env:COMPUTERNAME).ToLower())\IsAlive";Value="$ValueMetric"}
+    }
+
+   }
